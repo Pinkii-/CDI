1474246765526b31f37827413f41ea5bcf3527c8
Pinkii- <pinkiibiza@gmail.com>
2016-02-25 19:15:34 +0100

    entropy, joint entropy, conditional entropy

--- deliver1.py
+++ deliver1.py
@@ -1,5 +1,13 @@
+# from scipy import misc
+# lena = misc.lena()
+# import matplotlib.pyplot as plt
+# plt.imshow(lena,cmap=clt.cm.gray)
+
+
+
 import glob
 import unicodedata
+from math import log2
 
 def remove_accents(input_str):
     nfkd_form = unicodedata.normalize('NFKD', input_str)
@@ -37,8 +45,113 @@ datas = []
 for i in range(len(glob.glob(filepath))):
 	datas.append(clean_text(open(glob.glob(filepath)[i],'r').read()))
 
-print (datas[0])
 
-for x in set(datas[0]):
-	print (chr(x),end=' ')
+def entropy(txt):
 
+	for x in set(txt):
+		print (chr(x),end=' ')
+
+	print ('')	
+
+	countLeters = {};
+	for x in set(txt):
+		countLeters[x] = 0.0
+
+	total = 0.0
+	for x in txt:
+		total += 1
+		countLeters[x] += 1
+
+	ent = 0.0;
+	for x in countLeters:
+		ent += (countLeters[x]/total) * log2(1.0/(countLeters[x]/total))
+
+	return ent
+
+def joint_entropy(txt):
+	for x in set(txt):
+		print (chr(x),end=' ')
+
+	print('')
+
+	countLeters = {};
+	for x in set(txt):
+		countLeters[x] = {}
+		for y in set(txt):
+			countLeters[x][y] = 0.0
+
+	total = 0.0
+	for x in range(1,len(txt)):
+		total += 1.0
+		last = txt[x-1]
+		current = txt[x]
+		countLeters[last][current] += 1.0
+
+	ent = 0.0
+	for x in countLeters:
+		for y in countLeters:
+			if countLeters[x][y] != 0.0:
+				ent += (countLeters[x][y]/total) * log2(1.0/(countLeters[x][y]/total))
+
+	return ent/2
+
+
+def conditional_entropy1(txt,ltr):
+	countLeters = {};
+	for x in set(txt):
+		countLeters[x] = 0
+
+	total = 0.0
+	for x in range(1,len(txt)):
+		last = txt[x-1]
+		current = txt[x]
+		if chr(last) == ltr:
+			total += 1.0
+			countLeters[current] += 1.0
+
+	ent = 0.0
+	for x in countLeters:
+		if countLeters[x] != 0.0:
+			ent += (countLeters[x]/total) * log2(1.0/(countLeters[x]/total))
+
+	return ent
+
+def conditional_entropy(txt):
+	for x in set(txt):
+		print (chr(x),end=' ')
+
+	print('')
+
+	countLeters = {};
+	total = 0.0
+	totalCond = {};
+	for x in set(txt):
+		countLeters[x] = {}
+		totalCond[x] = 0
+		for y in set(txt):
+			countLeters[x][y] = 0.0
+
+	for x in range(1,len(txt)):
+		total += 1.0
+		last = txt[x-1]
+		current = txt[x]
+		countLeters[last][current] += 1.0
+		totalCond[last] += 1
+
+	ent = 0.0
+	for x in countLeters:
+		for y in countLeters:
+			if countLeters[x][y] != 0.0:
+				ent += (countLeters[x][y]/total) * log2(1.0/(countLeters[x][y]/totalCond[x]))
+
+	return ent
+
+
+# print (datas[0])
+
+
+
+# print (joint_entropy(datas[0]))
+
+print (conditional_entropy(datas[0]))
+print (joint_entropy(datas[0])*2 - entropy(datas[0]))
