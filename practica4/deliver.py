8c88300ecb1cc504f30f6329b8c898984b847d4f
Pinkii- <pinkiibiza@gmail.com>
2016-05-17 22:28:43 +0200

    LZW working "well"... At least can encode and decode a full character correctly

--- practica4/deliver.py
+++ practica4/deliver.py
@@ -132,9 +132,122 @@ def LZSS_decode(tok):
     return x
 
 
-def LZ78_encode():
+def LZ78_encode(txt):
     tokens = []
-    dic = {}
+    dic = list(sorted(set(txt)))
+    lastPosition = len(dic)-1
+    dic += [None]*((2**max((ceil(log2(len(dic)))),4) - len(dic)))
+   
+    # print(dic, len(dic))
+    print(lastPosition, dic)
+
+    incr = 0
+    for r in range(len(txt)):
+        r = r + incr
+        if (r >= len(txt)):
+            break
+
+        length = 0
+        i = -1
+        print(txt, "buclee")
+        print(txt[:r], txt[r:])
+        for x in range(lastPosition+1):
+            print ("   ",x, len(dic), dic[x])
+            if len(dic[x]) != 1 and (len(dic[x]) < length or len(dic[x]) + r > len(txt)-1):
+                continue
+            print("    ha entrado")
+            lengthAux = 0
+            util = True
+            for p in range(len(dic[x])):
+                if txt[r+p] == dic[x][p]:
+                    lengthAux += 1
+                else:
+                    util = False
+                    break
+            if util and lengthAux > length:
+                print (lengthAux)
+                length = lengthAux
+                a = txt[r+length:r+length+1]
+                i = x
+
+        print (" la a es ", a)
+
+        incr += length
+        lastPosition += 1
+        if lastPosition >= len(dic):
+            dic += [None]*len(dic)
+        dic[lastPosition] = txt[r:r+length+1]
+        print (dic)
+        tokens.append((i,a))
+        print (tokens)
+
+    return tokens
+
+def LZW_encode(txt):
+    tokens = []
+    dic = list(sorted(set(txt)))
+    lastPosition = len(dic)-1
+    dic += [None]*((2**max((ceil(log2(len(dic)))),4) - len(dic)))
+   
+    # print(dic, len(dic))
+    # print(lastPosition, dic)
+
+    incr = 0
+    for r in range(len(txt)):
+        r = r + incr
+        if (r >= len(txt)):
+            break
+
+        length = 0
+        i = -1
+        # print(txt, "buclee")
+        # print(txt[:r], txt[r:])
+        for x in range(lastPosition+1):
+            # print ("   ",x, len(dic), dic[x])
+            if len(dic[x]) != 1 and (len(dic[x]) < length or len(dic[x]) + r > len(txt)-1):
+                continue
+            # print("    ha entrado")
+            lengthAux = 0
+            util = True
+            for p in range(len(dic[x])):
+                if txt[r+p] == dic[x][p]:
+                    lengthAux += 1
+                else:
+                    util = False
+                    break
+            if util and lengthAux > length:
+                # print (lengthAux)
+                length = lengthAux
+                a = txt[r+length:r+length+1]
+                i = x
+
+        # print (" la a es ", a)
+
+        incr += length-1
+        lastPosition += 1
+        if lastPosition >= len(dic):
+            dic += [None]*len(dic)
+        dic[lastPosition] = txt[r:r+length+1]
+        # print (dic)
+        tokens.append((i))
+        # print (tokens)
+
+    return tokens
+
+def LZW_decode(dic,tok):
+    x = dic[tok[0]] 
+    dic += dic[tok[0]]
+    # print (dic)
+    for r in range(1,len(tok)):
+        # print ("tok", tok[r], "dic", dic[tok[r]])
+        # print (dic[len(dic)-1])
+        dic[len(dic)-1] += dic[tok[r]][0]
+        # print (dic[len(dic)-1])
+        x += dic[tok[r]]
+        dic.append(dic[tok[r]])
+        # print (dic)
+
+    return x
     
 
 
@@ -147,9 +260,11 @@ txt3 = 10*"0000000001"
 txt4 = "aaaaaaaaba"
 # txt4 = "abac"
 
-txt = "1234123412323123"
-txt = "AABCBBABC"
-txt = txt2
+# txt = "1234123412323123987"
+# txt = "AABCBBABC"
+txt = txt1
+
+## LZ77
 
 # _, tok = LZ77_encode(txt,4096,16)
 
@@ -158,9 +273,21 @@ txt = txt2
 # print (txt)
 # print(LZ77_decode(tok))
 
-_, tok = LZSS_encode(txt,16,8,2)
+## LZSS
 
-print (tok)
+# _, tok = LZSS_encode(txt,16,8,2)
 
-print (txt)
-print(LZSS_decode(tok))
+# print (tok)
+
+# print (txt)
+# print(LZSS_decode(tok))
+
+## LZ78
+
+# print(LZ78_encode(txt))
+
+## LZW
+
+# print()
+print("decoded ",LZW_decode(list(sorted(set(txt))),LZW_encode(txt)) == txt)
+# print("original",txt)
