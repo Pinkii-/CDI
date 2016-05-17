0dac3294319c8f6aa1e03af702315c9be959f317
Pinkii- <pinkiibiza@gmail.com>
2016-05-17 16:47:18 +0200

    LZSS

--- practica4/deliver.py
+++ practica4/deliver.py
@@ -2,7 +2,7 @@
 
 from math import log2, ceil
 
-def LZ77_enconde(txt,s,t):
+def LZ77_encode(txt,s,t):
     tokens = []
     incr = 0
     for r in range(len(txt)):
@@ -58,7 +58,71 @@ def LZ77_decode(tok):
             for l in range(length):
                 x+= x[-offset]
             x += letter
+    return x
 
+def LZSS_encode(txt,s,t,m):
+    tokens = []
+    incr = 0
+    for r in range(len(txt)):
+        # print(tokens)
+        r = r + incr
+        if (r >= len(txt)):
+            break
+
+        if r-s < 0:
+            searchBuffer = txt[0:r]
+        else:
+            searchBuffer = txt[r-s:r]    
+        lookaheadBuffer = txt[r:r+t]
+
+        offset = 0
+        length = 0
+        letter = lookaheadBuffer[0]
+
+        if len(lookaheadBuffer) == 1:
+            tokens.append((0,letter))
+            continue
+
+        print("searchBuffer" ,searchBuffer, "lookaheadBuffer", lookaheadBuffer)
+
+        for rSearchBuffer in range(len(searchBuffer)):
+            if searchBuffer[rSearchBuffer] == lookaheadBuffer[0]:
+                offset = len(searchBuffer) - rSearchBuffer
+                length = 1
+                window = searchBuffer + lookaheadBuffer
+                while length < len(lookaheadBuffer) and window[rSearchBuffer+length] == lookaheadBuffer[length]:
+                    length += 1
+                if length == len(lookaheadBuffer):
+                    length -= 1
+                letter = lookaheadBuffer[length]
+                break
+
+        
+        if length < m:
+            letter = lookaheadBuffer[0]
+            tokens.append((0,letter))
+        else:
+            incr += length-1
+            tokens.append((1,offset,length))
+            
+
+    # print(ceil(log2(s+1)),ceil(log2(t)),ceil(log2(len(set(txt)))))
+
+    bs = ceil(log2(s+1)) + ceil(log2(t)) + ceil(log2(len(set(txt)))) # Cambiar esto porque seguramente el enunciado está mal
+
+    return bs,tokens
+
+
+def LZSS_decode(tok):
+    x = ""
+    for t in tok:
+        if t[0] == 0:
+            x += t[1]
+        else:
+            offset = t[1]
+            length = t[2]
+            for l in range(length):
+                x+= x[-offset]
     return x
 
 
@@ -72,10 +136,18 @@ txt4 = "aaaaaaaaba"
 # txt4 = "abac"
 
 txt = "1234123412323123"
+txt = txt3
 
-_, tok = LZ77_enconde(txt,8,16)
+# _, tok = LZ77_encode(txt,8,16)
+
+# print (tok)
+
+# print (txt)
+# print(LZ77_decode(tok))
+
+_, tok = LZSS_encode(txt,16,8,2)
 
 print (tok)
 
 print (txt)
-print(LZ77_decode(tok))
+print(LZSS_decode(tok))
